550c17c2a85ae2d3d40f84621f2d0709667f08d0
Krutilla Zsolt <KRUTILLAZS@uniduna.hu>
2022-10-15 14:32:23 +0200

    Komment hozzáadása

--- src/main/java/dunaujvarosiegyetem/labor04/Labor04.java
+++ src/main/java/dunaujvarosiegyetem/labor04/Labor04.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 package dunaujvarosiegyetem.labor04;
-
+//Teszt
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Optional;
